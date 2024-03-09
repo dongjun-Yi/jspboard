5cca2a91dc0d3bdf1c89c50581ee0294c61cc150
dongjun-Yi <djyi0223@gmail.com>
2024-03-09 16:49:51 +0900

    게시글 Model 생성

---
+++ src/main/java/com/example/jspboard/model/PostModel.java
@@ -0,0 +1,15 @@
+package com.example.jspboard.model;
+
+import java.util.ArrayList;
+
+public class PostModel {
+    private static Long id;
+    public static ArrayList<Post> posts = new ArrayList();
+
+    public static Long generateId() {
+        if (id == null) {
+            id = 0L;
+        }
+        return id++;
+    }
+}
