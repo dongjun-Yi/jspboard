2e609790c203c04caf90afd5b5f33ff69a8d75b5
dongjun-Yi <djyi0223@gmail.com>
2024-03-09 16:51:05 +0900

    게시글 작성 비즈니스 로직& View 생성

---
+++ src/main/java/com/example/jspboard/controller/postAddServlet.java
@@ -0,0 +1,50 @@
+package com.example.jspboard.controller;
+
+import com.example.jspboard.model.Post;
+import com.example.jspboard.model.PostModel;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "postAddServlet", urlPatterns = "/post-add")
+public class postAddServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/post-add.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (!isPostExists(req)) {
+            Post post = new Post(PostModel.generateId(), req.getParameter("title"), req.getParameter("content"));
+            PostModel.posts.add(post);
+        }
+
+        String viewPath = "/WEB-INF/post-list.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+
+    private static boolean isPostExists(HttpServletRequest req) {
+        String getPostId = req.getParameter("id");
+        if (getPostId == null)
+            return false;
+        Long postId = Long.parseLong(getPostId);
+        for (Post p : PostModel.posts) {
+            if (postId == p.getId()) {
+                p.setTitle(req.getParameter("title"));
+                p.setContent(req.getParameter("content"));
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/webapp/WEB-INF/post-add.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dongjun
+  Date: 3/8/24
+  Time: 1:22 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="post-add" method="post">
+    title: <input type="text" name="title"/>
+    content: <input type="text" name="content"/>
+    <button type="submit">전송</button>
+</form>
+</body>
+</html>
