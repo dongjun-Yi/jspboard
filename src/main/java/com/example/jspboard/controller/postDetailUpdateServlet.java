88e2ab57841b22dd02d76c1a0b72401e48f8e430
dongjun-Yi <djyi0223@gmail.com>
2024-03-10 16:32:49 +0900

    게시글 상세페이지 수정화면 추가

---
+++ src/main/java/com/example/jspboard/controller/postDetailUpdateServlet.java
@@ -0,0 +1,29 @@
+package com.example.jspboard.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "postDetailUpdateServlet", urlPatterns = "/post-detail-update")
+public class postDetailUpdateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+
+        req.setAttribute("id", id);
+        req.setAttribute("title", title);
+        req.setAttribute("content", content);
+
+        String viewPath = "WEB-INF/post-detail-update.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
